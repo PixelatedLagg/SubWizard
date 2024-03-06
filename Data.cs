9cf2def81573788d150247e25911782f086b4faa
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2024-03-05 23:13:17 -0500

    barebones beginning

---
+++ Data.cs
@@ -0,0 +1,35 @@
+namespace SubWizard
+{
+    public static class Data
+    {
+        public static double[] LetterFreq = new double[]
+        {
+            8.2,
+            1.5,
+            2.8,
+            4.3,
+            12.7,
+            2.2,
+            2.0,
+            6.1,
+            7.0,
+            0.15,
+            0.77,
+            4.0,
+            2.4,
+            6.7,
+            7.5,
+            1.9,
+            0.095,
+            6.0,
+            6.3,
+            9.1,
+            2.8,
+            0.98,
+            2.4,
+            0.15,
+            2.0,
+            0.074
+        };
+    }
+}
